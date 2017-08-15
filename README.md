d8043c5854387518aa29ce97add6dec5a73e9169
Joachim Klahr <joachim@angelsware.com>
2017-08-15 16:00:56 +0200

    Added source files.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cordova-plugin-db
 Abstraction layer above WebSQL with the purpose of simplifying database structure changes.
+
+This project is in development. Use with caution.
+
+<code>Db.open("sample", "Sample", 1024 * 1024);
+Db.addStep(1, function() {
+	Db.transaction(function(tx) {
+		Db.execute(tx, "CREATE TABLE test (first INT);", [], null, null);
+		Db.execute(tx, "INSERT INTO test (first) VALUES('1');")
+	});
+});
+Db.addStep(2, function() {
+	Db.transaction(function(tx) {
+		Db.execute(tx, "ALTER TABLE test ADD COLUMN second INT;", [], null, null);
+	});
+});
+Db.addStep(3, function() {
+	Db.transaction(function(tx) {
+		Db.execute(tx, "ALTER TABLE test ADD COLUMN third INT;", [], null, null);
+	});
+});
+Db.run(function() {
+	console.log("success");
+}, function(error) {
+	console.warn(error.message);
+});</code>
---
+++ www/db.js
@@ -0,0 +1,69 @@
+function Db() {
+	var mWebSql = null;
+	var mSteps = [];
+
+	this.open = function(name, displayName, estimatedSize) {
+		mWebSql = window.openDatabase(name, "", displayName, estimatedSize);
+	};
+
+	this.addStep = function(step, func) {
+		mSteps[step] = func;
+	};
+
+	this.run = function(successCallback, errorCallback) {
+		var version = parseInt(mWebSql.version) || 0;
+		migrate(version + 1, successCallback, errorCallback);
+	};
+
+	var migrate = function(step, successCallback, errorCallback) {
+		if (mSteps[step]) {
+			mWebSql.changeVersion(mWebSql.version, String(step), function(t) {
+				mSteps[step](t);
+			}, function(error) {
+				if (errorCallback) {
+					errorCallback(error);
+				}
+			}, function() {
+				migrate(step + 1, successCallback, errorCallback);
+			});
+		} else {
+			if (successCallback) {
+				successCallback();
+			}
+		}
+	};
+
+	this.query = function(sql, params, successHandler, errorHandler) {
+		mWebSql.readTransaction(function(tx) {
+			tx.executeSql(sql, params, function(tx, result) {
+				if (successHandler) {
+					successHandler(result);
+				}
+			}, function(transaction, error) {
+				if (errorHandler) {
+					errorHandler(error);
+				}
+			});
+		});
+	};
+
+	this.transaction = function(callback) {
+		mWebSql.transaction(function(tx) {
+			callback(tx);
+		});
+	};
+
+	this.execute = function(tx, sql, params, successHandler, errorHandler) {
+		tx.executeSql(sql, params, function(tx, result) {
+			if (successHandler) {
+				successHandler(result);
+			}
+		}, function(transaction, error) {
+			if (errorHandler) {
+				errorHandler(error);
+			}
+		});
+	};
+}
+
+module.exports = new Db();
